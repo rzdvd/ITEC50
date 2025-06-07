1cfbdd8cd87e608fa02e0bb428aef661dbbb3d80
rzdvd <ruizdavidcabrera@gmail.com>
2025-06-07 16:11:12 +0800

    -added php files

---
+++ plans.php
@@ -0,0 +1,33 @@
+<?php
+$pageTitle = "Plans";
+$pageId = "plans";
+include 'includes/header.php';
+?>
+
+<main>
+    <header>
+        <h2>Week 15, Day 2</h2>
+        <div class="date">Tue, May 20</div>
+    </header>
+
+    <section class="tags">
+        <span class="focus-tag">FOCUS: CORE</span>
+        <span class="goal-tag">GOAL: 5 Excerises</span>
+    </section>
+
+    <div class="day-tabs">
+        <button class="day-tab">Sun</button>
+        <button class="day-tab">Mon</button>
+        <button class="day-tab active">Tue</button>
+        <button class="day-tab">Wed</button>
+        <button class="day-tab">Thu</button>
+        <button class="day-tab">Fri</button>
+        <button class="day-tab">Sat</button>
+    </div>
+
+    <section id="workout-plan">
+        
+    </section>
+</main>
+
+<?php include 'includes/footer.php'; ?>
