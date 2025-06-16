29cb80b3aca47978232375da529dad1c466e1c4b
rzdvd <ruizdavidcabrera@gmail.com>
2025-06-17 03:50:24 +0800

    fix nav bar, font styles

--- workouts.php
+++ workouts.php
@@ -23,57 +23,58 @@ $pageId = 'workouts';
 <body id="<?php echo $pageId ?? ''; ?>">
 
 <div class="nav">
-    <ul>
-        <li>
-            <a href="home.php">
-                <div>
-                    <img src="assets/images/home-icon.webp" alt="">
-                    <p>Home</p>
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="workouts.php">
-                <div>
-                    <img src="assets/images/workouts-icon.webp" alt="">
-                    <p>Workouts</p>
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="plans.php">
-                <div>
-                    <img src="assets/images/plans-icon.webp" alt="">
-                    <p>Plans</p>
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="history.php">
-                <div>
-                    <img src="assets/images/history-icon.webp" alt="">
-                    <p>History</p>
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="progress.php">
-                <div>
-                    <img src="assets/images/progress-icon.webp" alt="">
-                    <p>Progress</p>
-                </div>
-            </a>
-        </li>
-        <li>
-            <a href="profile.html">
-                <div>
-                    <img src="assets/images/profile-icon.svg" alt="">
-                    <p>Profile</p>
-                </div>
-            </a>
-        </li>
-    </ul>
-</div>
+        <ul>
+            <li>
+                <a href="home.php" class="<?= ($pageId == 'home') ? 'active' : '' ?>">
+                    <div>
+                        <img src="assets/images/home-icon.webp" alt="">
+                        <p>Home</p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="workouts.php" class="<?= ($pageId == 'workouts') ? 'active' : '' ?>">
+                    <div>
+                        <img src="assets/images/workouts-icon.webp" alt="">
+                        <p>Workouts</p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="plans.php" class="<?= ($pageId == 'plans') ? 'active' : '' ?>" >
+                    <div>
+                        <img src="assets/images/plans-icon.webp" alt="">
+                        <p>Plans</p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="history.php" class="<?= ($pageId == 'history') ? 'active' : '' ?>" >
+                    <div>
+                        <img src="assets/images/history-icon.webp" alt="">
+                        <p>History</p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="progress.php" class="<?= ($pageId == 'progress') ? 'active' : '' ?>">
+                    <div>
+                        <img src="assets/images/progress-icon.webp" alt="">
+                        <p>Progress</p>
+                    </div>
+                </a>
+            </li>
+            <li>
+                <a href="profile.php" class="<?= ($pageId == 'profile') ? 'active' : '' ?>">
+                    <div>
+                        <img src="assets/images/profile-icon.svg" alt="">
+                        <p>Profile</p>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+    
     <div class="content">
         <div class="workout-main">
             <h2 class="section-title">TARGET BODY PART</h2>
